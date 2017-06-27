ab4c9ed33d7801307f52bc3ae1837051d7797a21
Yash Jain <techmahindraashhy@gmail.com>
2017-06-27 18:27:47 +0530

    Initial commit

---
+++ app/src/main/java/aiims/survey/techmahindra/aiimssurvey/Survey.java
@@ -0,0 +1,74 @@
+package aiims.survey.techmahindra.aiimssurvey;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+
+import java.util.Locale;
+
+/**
+ * Created by yashjain on 6/26/17.
+ */
+
+public class Survey {
+
+
+    private String sId;
+    private int version;
+    private String title;
+    private String description;
+    private int totalQuestions;
+    private String language;
+
+    public Survey(){
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getTotalQuestions() {
+        return totalQuestions;
+    }
+
+    public void setTotalQuestions(int totalQuestions) {
+        this.totalQuestions = totalQuestions;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getsId() {
+        return sId;
+    }
+
+    public void setsId(String sId) {
+        this.sId = sId;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+}
